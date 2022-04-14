b87c402d4d6f41ac2293d92a970ae7981afd2dca
Yohta Ito <yohta.ito@orithankyou.co.jp>
2022-04-14 16:16:32 +0900

    nvim: telescope

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -6,124 +6,124 @@ local trouble = require("trouble.providers.telescope")
 local transform_mod = require('telescope.actions.mt').transform_mod
 
 require("telescope").setup({
-    defaults = {
-        -- dynamic_preview_title = true,
-        -- borderchars = { "▔", "▕", "▁", "▏", "🭽", "🭾", "🭿", "🭼" },
-        -- layout_strategy = "flex",
-        layout_config = {
-            vertical = {
-                preview_height = 0.5,
-            },
-            flex = {
-                flip_columns = 160,
-            },
-            horizontal = {
-                preview_width = 0.5,
-            },
-            height = 0.85,
-            width = 0.85,
-            preview_cutoff = 0,
-        },
-        file_ignore_patterns = { "node_modules", ".git" },
-        path_display = {
-            truncate = 1,
-        },
-        set_env = {
-            ["COLORTERM"] = "truecolor",
-        }, -- default = nil,
-        -- file_previewer = previewers.cat.new,
-        -- grep_previewer = previewers.vim_buffer_vimgrep.new,
-        -- qflist_previewer = previewers.qflist.new,
-        history = {
-            mappings = {
-                i = {
-                    ["<C-Down>"] = actions.cycle_history_next,
-                    ["<C-Up>"] = actions.cycle_history_prev,
-                },
-            },
-        },
-        mappings = {
-            i = {
-                ["<C-j>"] = actions.move_selection_next,
-                ["<C-k>"] = actions.move_selection_previous,
-                ["<Tab>"] = actions.move_selection_previous,
-                ["<S-Tab>"] = actions.move_selection_next,
-                ["<C-z>"] = actions.toggle_selection,
-                ["<C-s>"] = actions.select_horizontal,
-                ["<C-x>"] = trouble.smart_open_with_trouble,
-                ["<M-l>"] = function(prompt_bufnr)
-                    actions.smart_send_to_loclist(prompt_bufnr)
-                    require("trouble").open("loclist")
-                end,
-                ["<M-a>"] = actions.toggle_all,
-                ["<C-Down>"] = actions.cycle_history_next,
-                ["<C-Up>"] = actions.cycle_history_prev,
-            },
-            n = {
-                ["<C-z>"] = actions.toggle_selection,
-                ["<C-s>"] = actions.select_horizontal,
-                ["<C-x>"] = trouble.smart_open_with_trouble,
-                ["<M-a>"] = actions.toggle_all,
-                ["<M-l>"] = function(prompt_bufnr)
-                    actions.smart_send_to_loclist(prompt_bufnr)
-                    require("trouble").open("loclist")
-                end,
-                ["<C-Down>"] = actions.cycle_history_next,
-                ["<C-Up>"] = actions.cycle_history_prev,
-            },
-        },
+  defaults = {
+    -- dynamic_preview_title = true,
+    -- borderchars = { "▔", "▕", "▁", "▏", "🭽", "🭾", "🭿", "🭼" },
+    -- layout_strategy = "flex",
+    layout_config = {
+      vertical = {
+        preview_height = 0.5,
+      },
+      flex = {
+        flip_columns = 160,
+      },
+      horizontal = {
+        preview_width = 0.5,
+      },
+      height = 0.85,
+      width = 0.85,
+      preview_cutoff = 0,
     },
-    pickers = {
-        find_files = {
-            follow = true,
-            theme = "dropdown",
-        },
-        buffers = {
-            sort_mru = true,
-        },
-        lsp_code_actions = themes.get_cursor(),
-        lsp_range_code_actions = themes.get_cursor(),
-        lsp_references = {
-            timeout = 100000,
-        },
-        lsp_definitions = {
-            timeout = 100000,
-        },
-        lsp_type_definitions = {
-            timeout = 100000,
-        },
-        lsp_implementations = {
-            timeout = 100000,
-        },
-        lsp_workspace_symbols = {
-            timeout = 100000,
-        },
-        lsp_dynamic_workspace_symbols = {
-            timeout = 100000,
-        },
+    file_ignore_patterns = { "node_modules", ".git" },
+    path_display = {
+      truncate = 1,
     },
-    extension = {
-        file_browser = {
-            hidden = true,
-            depth = 2,
+    set_env = {
+      ["COLORTERM"] = "truecolor",
+    }, -- default = nil,
+    -- file_previewer = previewers.cat.new,
+    -- grep_previewer = previewers.vim_buffer_vimgrep.new,
+    -- qflist_previewer = previewers.qflist.new,
+    history = {
+      mappings = {
+        i = {
+          ["<C-Down>"] = actions.cycle_history_next,
+          ["<C-Up>"] = actions.cycle_history_prev,
         },
+      },
     },
+    mappings = {
+      i = {
+        ["<C-j>"] = actions.move_selection_next,
+        ["<C-k>"] = actions.move_selection_previous,
+        ["<Tab>"] = actions.move_selection_previous,
+        ["<S-Tab>"] = actions.move_selection_next,
+        ["<C-z>"] = actions.toggle_selection,
+        ["<C-s>"] = actions.select_horizontal,
+        ["<C-x>"] = trouble.smart_open_with_trouble,
+        ["<M-l>"] = function(prompt_bufnr)
+          actions.smart_send_to_loclist(prompt_bufnr)
+          require("trouble").open("loclist")
+        end,
+        ["<M-a>"] = actions.toggle_all,
+        ["<C-Down>"] = actions.cycle_history_next,
+        ["<C-Up>"] = actions.cycle_history_prev,
+      },
+      n = {
+        ["<C-z>"] = actions.toggle_selection,
+        ["<C-s>"] = actions.select_horizontal,
+        ["<C-x>"] = trouble.smart_open_with_trouble,
+        ["<M-a>"] = actions.toggle_all,
+        ["<M-l>"] = function(prompt_bufnr)
+          actions.smart_send_to_loclist(prompt_bufnr)
+          require("trouble").open("loclist")
+        end,
+        ["<C-Down>"] = actions.cycle_history_next,
+        ["<C-Up>"] = actions.cycle_history_prev,
+      },
+    },
+  },
+  pickers = {
+    find_files = {
+      follow = true,
+      theme = "dropdown",
+    },
+    buffers = {
+      sort_mru = true,
+    },
+    lsp_code_actions = themes.get_cursor(),
+    lsp_range_code_actions = themes.get_cursor(),
+    lsp_references = {
+      timeout = 100000,
+    },
+    lsp_definitions = {
+      timeout = 100000,
+    },
+    lsp_type_definitions = {
+      timeout = 100000,
+    },
+    lsp_implementations = {
+      timeout = 100000,
+    },
+    lsp_workspace_symbols = {
+      timeout = 100000,
+    },
+    lsp_dynamic_workspace_symbols = {
+      timeout = 100000,
+    },
+  },
+  extension = {
+    file_browser = {
+      hidden = true,
+      depth = 2,
+    },
+  },
 })
 
 local copts = { noremap = true }
 vim.api.nvim_set_keymap("n", "<leader>ff", "<cmd>Telescope find_files<CR>", copts)
 vim.api.nvim_set_keymap("n", "<leader>o", "<cmd>Telescope find_files<CR>", copts)
 vim.api.nvim_set_keymap(
-    "n",
-    "<leader>f.",
-    "<cmd>lua require'telescope'.extensions.file_browser.file_browser()<CR>",
-    copts
+  "n",
+  "<leader>f.",
+  "<cmd>lua require'telescope'.extensions.file_browser.file_browser()<CR>",
+  copts
 )
 vim.api.nvim_set_keymap("n", "<leader>fl", "<cmd>Telescope current_buffer_fuzzy_find<CR>", copts)
 vim.api.nvim_set_keymap("n", "<leader>fq", "<cmd>Telescope quickfix<CR>", copts)
-vim.api.nvim_set_keymap("n", "<leader>h", "<cmd>Telescope oldfiles<CR>", copts)
+vim.api.nvim_set_keymap("n", "<leader>fh", "<cmd>Telescope oldfiles<CR>", copts)
 vim.api.nvim_set_keymap("n", "<leader>fr", "<cmd>Telescope frecency<CR>", copts)
-vim.api.nvim_set_keymap("n", "<leader>b", "<cmd>Telescope buffers<CR>", copts)
+vim.api.nvim_set_keymap("n", "<leader>fb", "<cmd>Telescope buffers<CR>", copts)
 vim.api.nvim_set_keymap("n", "<leader>fg", "<cmd>Telescope live_grep<CR>", copts)
 -- vim.api.nvim_set_keymap("n", "<leader><space>", "<cmd>Telescope commands<CR>", copts)
 vim.api.nvim_set_keymap("n", "<leader>fc", "<cmd>Telescope commands<CR>", copts)
