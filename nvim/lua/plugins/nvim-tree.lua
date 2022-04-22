a6de50fc47632031319f402e15cae570082ce94f
Yohta Ito <i@yxhta.com>
2022-04-22 15:36:35 +0900

    nvim: nvim-tree

--- nvim/lua/plugins/nvim-tree.lua
+++ nvim/lua/plugins/nvim-tree.lua
@@ -2,40 +2,40 @@ local tree_cb = require("nvim-tree.config").nvim_tree_callback
 local copts = { noremap = true }
 
 vim.g.nvim_tree_show_icons = {
-	git = 1,
-	folders = 1,
-	files = 1,
-	folder_arrows = 1,
+    git = 1,
+    folders = 1,
+    files = 1,
+    folder_arrows = 1,
 }
 
 vim.g.nvim_tree_icons = {
-	default = "",
-	symlink = "",
-	git = {
-		unstaged = "✗",
-		staged = "✓",
-		unmerged = "",
-		renamed = "➜",
-		untracked = "★",
-		deleted = "",
-		ignored = "◌",
-	},
-	folder = {
-		arrow_open = "",
-		arrow_closed = "",
-		default = "",
-		open = "",
-		empty = "",
-		empty_open = "",
-		symlink = "",
-		symlink_open = "",
-	},
-	lsp = {
-		hint = "",
-		info = "",
-		warning = "",
-		error = "",
-	},
+    default = "",
+    symlink = "",
+    git = {
+        unstaged = "✗",
+        staged = "✓",
+        unmerged = "",
+        renamed = "➜",
+        untracked = "★",
+        deleted = "",
+        ignored = "◌",
+    },
+    folder = {
+        arrow_open = "",
+        arrow_closed = "",
+        default = "",
+        open = "",
+        empty = "",
+        empty_open = "",
+        symlink = "",
+        symlink_open = "",
+    },
+    lsp = {
+        hint = "",
+        info = "",
+        warning = "",
+        error = "",
+    },
 }
 
 -- vim.g.nvim_tree_ignore                 = {".git", "node_modules", ".cache"}
@@ -51,34 +51,38 @@ vim.g.nvim_tree_allow_resize = 1
 vim.g.nvim_tree_add_trailing = 1 -- append a trailing slash to folder names
 
 require("nvim-tree").setup({
-	disable_netrw = false,
-	hijack_netrw = true,
-	open_on_setup = false,
-	ignore_ft_on_setup = {},
-	auto_close = false,
-	open_on_tab = false,
-	hijack_cursor = false,
-	update_cwd = true,
-	hide_dotfiles = false,
-	-- ignore  = {".git", "node_modules", ".cache"},
-	diagnostics = { enable = false },
-	git = { enabled = true, ignore = true },
-
-	update_to_buf_dir = {
-	    enable = true,
-	    auto_open = true
-	},
-	update_focused_file = {
-		enable = true,
-		update_cwd = false,
-		ignore_list = {},
-	},
-	system_open = {
-		cmd = nil,
-		args = {},
-	},
+    disable_netrw = false,
+    hijack_netrw = true,
+    open_on_setup = false,
+    ignore_ft_on_setup = {},
+    auto_close = false,
+    open_on_tab = false,
+    hijack_cursor = false,
+    update_cwd = true,
+    hide_dotfiles = false,
+    -- ignore  = {".git", "node_modules", ".cache"},
+    diagnostics = { enable = false },
+    git = { enabled = true, ignore = true },
+    -- actions = {
+    --     open_file = {
+    --         quit_on_open = true,
+    --     },
+    -- },
+    update_to_buf_dir = {
+        enable = true,
+        auto_open = true
+    },
+    update_focused_file = {
+        enable = true,
+        update_cwd = false,
+        ignore_list = {},
+    },
+    system_open = {
+        cmd = nil,
+        args = {},
+    },
 })
 
-vim.api.nvim_set_keymap("n", "<leader>n", "<cmd>NvimTreeOpen<CR>", copts)
-vim.api.nvim_set_keymap("n", "<leader>nt", "<cmd>NvimTreeToggle<CR>", copts)
+-- vim.api.nvim_set_keymap("n", "<leader>n", "<cmd>NvimTreeOpen<CR>", copts)
+vim.api.nvim_set_keymap("n", "<leader>n", "<cmd>NvimTreeToggle<CR>", copts)
 vim.api.nvim_set_keymap("n", "<leader>nf", "<cmd>NvimTreeFindFile<CR>", copts)
