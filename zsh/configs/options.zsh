aaa4ccb8c16ce6ef7df5e46722a2229b9b4e2964
Yohta Ito <yohta.ito@orithankyou.co.jp>
2022-10-04 13:58:45 +0900

    zsh: configs and options

--- zsh/configs/options.zsh
+++ zsh/configs/options.zsh
@@ -1,9 +1,9 @@
 # awesome cd movements from zshkit
-setopt autocd autopushd pushdminus pushdsilent pushdtohome cdablevars
-DIRSTACKSIZE=5
+# setopt autocd autopushd pushdminus pushdsilent pushdtohome cdablevars
+# DIRSTACKSIZE=5
 
 # Enable extended globbing
-setopt extendedglob
+# setopt extendedglob
 
 # Allow [ or ] whereever you want
-unsetopt nomatch
+# unsetopt nomatch
