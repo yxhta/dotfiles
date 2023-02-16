1dbc03e68b780388637c4c79f6589dadfa757c46
Yohta Ito <i@yxhta.com>
2023-02-16 16:43:00 +0900

    nvim: init.lua

--- nvim/init.lua
+++ nvim/init.lua
@@ -4,27 +4,31 @@
 -- Startup
 require("impatient")
 
+-- Disable netrw at the very start of your init.lua for nvim-tree
+vim.g.loaded_netrw = 1
+vim.g.loaded_netrwPlugin = 1
+
 -- Plugins
 require("plugins")
 
 -- Colorscheme
 vim.opt.laststatus = 3
 require("kanagawa").setup({
-  dimInactive = true,
-  globalStatus = true
+    dimInactive = true,
+    globalStatus = true
 })
-vim.cmd("colorscheme kanagawa")
+vim.cmd("colorscheme nightfox")
 require("colors").overrides()
 
 vim.cmd("set clipboard+=unnamedplus")
 
 require('lualine').setup()
 
--- general configurations
+-- General configurations
 require("options")
 
 -- Functions, Commands, Autocommands
 vim.cmd("source ~/.config/nvim/viml/autocommands.vim")
 
--- Mappings
+-- Key mappings
 vim.cmd("source ~/.config/nvim/viml/mappings.vim")
