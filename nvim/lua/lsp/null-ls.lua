e93ecd8eb68c1219224c51967018a417233c4a62
Yohta Ito <i@yxhta.com>
2023-02-16 16:41:21 +0900

    nvim: null-ls disable golangci-lint

--- nvim/lua/lsp/null-ls.lua
+++ nvim/lua/lsp/null-ls.lua
@@ -1,14 +1,17 @@
 local null_ls = require("null-ls")
 
 local sources = {
-    null_ls.builtins.formatting.prettier,
-    null_ls.builtins.diagnostics.golangci_lint,
+    -- null_ls.builtins.formatting.prettier,
+    null_ls.builtins.formatting.prettier.with {
+        prefer_local = "node_modules/.bin",
+    },
+    -- null_ls.builtins.diagnostics.golangci_lint,
     null_ls.builtins.code_actions.gitsigns,
 }
 
 null_ls.setup({
     sources = sources,
     on_attach = require("lsp.lsp-config").on_attach,
-    -- capabilities = require'lsp.lsp-config'.capabilities
+    capabilities = require 'lsp.lsp-config'.capabilities,
     debug = false,
 })
