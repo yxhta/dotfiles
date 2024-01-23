afd56764ce790078e175c3fe03cd2baf6f228d3d
Yota Ito <i@yxhta.com>
2024-01-23 23:51:45 +0900

    nvim: update

--- nvim/lua/lsp/null-ls.lua
+++ nvim/lua/lsp/null-ls.lua
@@ -2,16 +2,16 @@ local null_ls = require("null-ls")
 
 local sources = {
     null_ls.builtins.formatting.prettierd,
-    -- null_ls.builtins.diagnostics.golangci_lint,
-    null_ls.builtins.diagnostics.eslint_d.with({
+    null_ls.builtins.diagnostics.eslint.with({
         diagnostics_format = '[eslint] #{m}\n(#{c})'
     }),
+    -- null_ls.builtins.diagnostics.golangci_lint,
     null_ls.builtins.code_actions.gitsigns,
-    null_ls.builtins.completion.spell,
+    -- null_ls.builtins.completion.spell,
 }
 null_ls.setup({
     sources = sources,
     on_attach = require("lsp.lsp-config").on_attach,
     capabilities = require 'lsp.lsp-config'.capabilities,
-    debug = true,
+    debug = false,
 })
--- nvim/lua/plugins/indent-blankline.lua
+++ nvim/lua/plugins/indent-blankline.lua
@@ -1,14 +1,15 @@
-require("indent_blankline").setup {
+require("ibl").setup {
     buftype_exclude = {"terminal", "prompt", "nofile"},
     filetype_exclude = {
         'help', 'dashboard', 'Trouble', 'dap.*', 'NvimTree',
         "packer"
     },
     show_current_context = true,
-    show_current_context_start = false,
+    -- show_current_context_start = false,
     show_trailing_blankline_indent = false,
-    use_treesitter = true,
-    context_patterns = {
-        'class', 'function', 'method', '.*_statement', 'table'
-    }
+    -- use_treesitter = true,
+    -- use_treesitter_scope = true
+    -- context_patterns = {
+    --     'class', 'func', 'method', '.*_statement', 'table'
+    -- }
 }
